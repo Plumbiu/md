392264cb355a128bd5610727c48a422f8d29e3dd
Plumbiu <3434909403@qq.com>
2023-10-25 14:55:03 +0800

    feat: add benchmark and test

---
+++ benchmark/md2toc/index.bench.ts
@@ -0,0 +1,25 @@
+import { md2toc as v0_3_3 } from './v0_3_3'
+import { md2toc } from '../../src/md'
+import { bench } from 'vitest'
+import fs from 'node:fs'
+import { dirname, join } from 'node:path'
+
+// 文件的路径
+const __dirname = dirname(__filename)
+const md = fs.readFileSync(join(__dirname, './test.md'), 'utf-8')
+
+bench(
+  'latest',
+  () => {
+    md2toc(md)
+  },
+  { time: 100 },
+)
+
+bench(
+  'v0_3_3',
+  () => {
+    v0_3_3(md)
+  },
+  { time: 100 },
+)
---
+++ test/md2toc.test.ts
@@ -0,0 +1,48 @@
+import { md2toc } from 'src'
+import { expect, test } from 'vitest'
+
+
+test('base test', () => {
+  const md = `
+# hello
+
+## world
+
+你好
+
+## foo
+
+# bar
+`
+  const toc = md2toc(md)
+
+  expect(toc).toEqual([
+    { level: 1, content: 'hello', id: 'hello' },
+    { level: 2, content: 'world', id: 'world' },
+    { level: 2, content: 'foo', id: 'foo' },
+    { level: 1, content: 'bar', id: 'bar' },
+  ])
+})
+
+test('with \s test', () => {
+  const md = `
+# hello world
+
+## world
+
+你好
+
+## foo
+
+# bar
+`
+  const toc = md2toc(md)
+
+  expect(toc).toEqual([
+    { level: 1, content: 'hello world', id: 'helloworld' },
+    { level: 2, content: 'world', id: 'world' },
+    { level: 2, content: 'foo', id: 'foo' },
+    { level: 1, content: 'bar', id: 'bar' },
+  ])
+})
+
